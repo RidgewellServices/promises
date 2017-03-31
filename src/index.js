3b2adb450b504dbd56f71724c0539308f21d29cd
Ramona Ridgewell <ramona@ridgewellservices.com>
2017-03-31 14:40:51 -0700

    initial checkin

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    var args = process.argv;
+    if (args[2] && args[2] === '--v' ) {
+        console.log('Es6CliSkeleton - version 1.0.0');
+    } else {
+        console.log('Hello');
+    }
+}
+
+main();
