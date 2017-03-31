0502202a3cc85f9f95a9d665cfa5cde99e6fc376
Ramona Ridgewell <ramona@ridgewellservices.com>
2017-03-31 15:23:42 -0700

    added Babel components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import hello from './hello';
+
 function main() {
     var args = process.argv;
     if (args[2] && args[2] === '--v' ) {
         console.log('Es6CliSkeleton - version 1.0.0');
     } else {
-        console.log('Hello');
+        console.log(hello);
     }
 }
 
