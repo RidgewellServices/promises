c92106ced26ee55815a88da4f046625420f385dc
Ramona Ridgewell <ramona@ridgewellservices.com>
2017-04-11 18:08:25 -0700

    add promise

--- src/hello.js
+++ src/hello.js
@@ -1,3 +1,11 @@
-const hello = "Hello";
+const getWorld = () => new Promise((resolve) => {
+  setTimeout(() => resolve(' world'), 500);
+});
+
+const hello = () => new Promise((resolve) => {
+  getWorld().then(result => {
+    resolve('hello' + result);
+  });
+});
 
 export default hello;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import hello from './hello';
 
+const sayHello = (message) => {
+  console.log(message);
+};
+
 function main() {
     var args = process.argv;
     if (args[2] && args[2] === '--v' ) {
         console.log('Es6CliSkeleton - version 1.0.0');
     } else {
-        console.log(hello);
+        hello().then(res => sayHello(res));
     }
 }
 
