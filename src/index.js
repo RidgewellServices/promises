0f09fd673fb37f2ae28ae7f967a3d29adb92fbd8
Ramona Ridgewell <ramona@ridgewellservices.com>
2017-04-12 12:40:03 -0700

    update main() to arrow function syntax

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ const sayHello = (message) => {
   console.log(message);
 };
 
-function main() {
+const main = () => {
     var args = process.argv;
     if (args[2] && args[2] === '--v' ) {
         console.log('Es6CliSkeleton - version 1.0.0');
     } else {
         hello().then(res => sayHello(res));
     }
-}
+};
 
 main();
